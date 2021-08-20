f87088943857f0891d8e72d58ba6922be9438ba9
jannat456nur <jannatjenny22@gmail.com>
2021-08-20 12:41:38 -0700

    third-commit

--- mac.js
+++ mac.js
@@ -12,4 +12,40 @@ document.getElementById('case-down').addEventListener('click', function () {
     const caseNumber = caseInput.innerText;
     console.log(caseNumber)
     caseInput.innerText = parseInt(caseNumber) - 180;
-})
+});
+//for ssd 256
+document.getElementById('memory-256').addEventListener('click', function () {
+    const caseInput = document.getElementById('ssd');
+    const caseNumber = caseInput.innerText;
+    console.log(caseNumber)
+    caseInput.innerText = parseInt(caseNumber) - 180;
+});
+//for ssd 521
+document.getElementById('memory-512').addEventListener('click', function () {
+    const caseInput = document.getElementById('ssd');
+    const caseNumber = caseInput.innerText;
+    console.log(caseNumber)
+    caseInput.innerText = parseInt(caseNumber) + 100;
+});
+//for ssd 1TB
+document.getElementById('memory-1TB').addEventListener('click', function () {
+    const caseInput = document.getElementById('ssd');
+    const caseNumber = caseInput.innerText;
+    console.log(caseNumber)
+    caseInput.innerText = parseInt(caseNumber) - 100 + 280;
+});
+//for delivery cost 25 aug
+document.getElementById('25-aug').addEventListener('click', function () {
+    const caseInput = document.getElementById('delivery');
+    const caseNumber = caseInput.innerText;
+    console.log(caseNumber)
+    caseInput.innerText = parseInt(caseNumber) - 20;
+});
+//for 16aug
+
+document.getElementById('16-aug').addEventListener('click', function () {
+    const caseInput = document.getElementById('delivery');
+    const caseNumber = caseInput.innerText;
+    console.log(caseNumber)
+    caseInput.innerText = parseInt(caseNumber) + 20;
+});
